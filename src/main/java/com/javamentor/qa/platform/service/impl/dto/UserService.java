3f58b1c88c2a6cd0a28800d2dd92da33967db512
Almaz <zalyalov.almaz2016@gmail.com>
2021-08-16 17:27:42 +0300

    UserService and RoleService are moved to abstract/model from impl/dto

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.javamentor.qa.platform.service.impl.dto;
-
-import com.javamentor.qa.platform.models.entity.user.User;
-import com.javamentor.qa.platform.service.abstracts.model.ReadWriteService;
-
-public interface UserService extends ReadWriteService<User, Long> {
-}
