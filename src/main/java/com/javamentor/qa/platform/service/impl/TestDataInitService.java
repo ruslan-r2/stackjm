8d16d0884737cdae536b2bda772e559111ee1aa3
bulaton <zavtra.v.dva@gmail.com>
2021-08-16 19:50:17 +0300

    Add Admin User Data Initialization

--- src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
@@ -1,13 +1,18 @@
 package com.javamentor.qa.platform.service.impl;
 
 
+import com.javamentor.qa.platform.models.entity.user.Role;
+import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.impl.dto.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 public class TestDataInitService {
+
 
     private UserService userService;
 
@@ -18,7 +23,24 @@ public class TestDataInitService {
 
     @Transactional
     public void createEntity() {
+        Role adminRole = new Role ("ADMIN");
 
+        User admin = new User();
+        admin.setEmail("admin@admin.com");
+        admin.setPassword("admin");
+        admin.setFullName("John Smith");
+        admin.setPersistDateTime(LocalDateTime.now());
+        admin.setIsEnabled(true);
+        admin.setCity("CitiName");
+        admin.setImageLink("image.link.com");
+        admin.setLinkSite("sitename.admin.com");
+        admin.setLinkGitHub("admin.github.com");
+        admin.setLinkVk("vk.com/admin");
+        admin.setAbout("It'sa me, Mario!");
+        admin.setNickname("IamTheLaw");
+        admin.setLastUpdateDateTime(LocalDateTime.now());
+        admin.setRole(adminRole);
+        userService.persist(admin);
     }
 
 }
