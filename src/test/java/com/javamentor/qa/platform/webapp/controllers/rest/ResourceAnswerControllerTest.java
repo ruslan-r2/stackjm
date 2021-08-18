1e4027a7e3f77e5998897ab02bcf307cc57d9621
Almaz <zalyalov.almaz2016@gmail.com>
2021-08-18 17:57:20 +0300

    integration tests added

--- src/test/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerControllerTest.java
@@ -1,10 +1,15 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
+import com.javamentor.qa.platform.dao.impl.model.AnswerDaoImpl;
+import com.javamentor.qa.platform.dao.impl.model.QuestionDaoImpl;
+import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,16 +20,20 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureMockMvc
 class ResourceAnswerControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private ResourceAnswerController resourceAnswerController;
-
     @Test
-    void getQuestionById() {
+    void getQuestionById(Long id) {
+        ResourceAnswerController controller = new ResourceAnswerController(new QuestionDaoImpl(), new AnswerDaoImpl());
+        Question response = controller.getQuestionById(id).getBody();
+        assert response != null;
+        assertEquals(response.getId(), id);
     }
 
     @Test
-    void deleteAnswerById() {
+    void deleteAnswerById(Long questionId, Long answerId) {
+        ResourceAnswerController controller = new ResourceAnswerController(new QuestionDaoImpl(), new AnswerDaoImpl());
+        Answer response = controller.deleteAnswerById(questionId, answerId).getBody();
+        assert response != null;
+        assertEquals(response.getQuestion().getId(), questionId);
+        assertEquals(response.getId(), answerId);
     }
 }
