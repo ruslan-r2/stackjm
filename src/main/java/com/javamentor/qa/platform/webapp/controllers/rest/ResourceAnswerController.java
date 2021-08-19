446160512aa1b3b5c5214155d4d7cf1de08569cd
Almaz <zalyalov.almaz2016@gmail.com>
2021-08-19 20:09:14 +0300

    ResourceAnswerController fixed

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
@@ -4,6 +4,8 @@ import com.javamentor.qa.platform.dao.abstracts.model.AnswerDao;
 import com.javamentor.qa.platform.dao.abstracts.model.QuestionDao;
 import com.javamentor.qa.platform.models.entity.question.Question;
 import com.javamentor.qa.platform.models.entity.question.answer.Answer;
+import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
+import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,32 +14,21 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/user/question/")
+@RequestMapping("api/user/question/{questionId}/answer")
 @AllArgsConstructor
 public class ResourceAnswerController {
 
-    private final QuestionDao questionDao;
-    private final AnswerDao answerDao;
+    private final QuestionService questionService;
+    private final AnswerService answerService;
 
-    @GetMapping("/{questionId}")
-    public ResponseEntity<Question> getQuestionById(@PathVariable(name = "questionId") Long questionId) {
-        Question result;
-        if (questionDao.getById(questionId).isPresent()) {
-            result = questionDao.getById(questionId).get();
-            return new ResponseEntity<>(result, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-    }
-
-    @DeleteMapping("/{questionId}/answer/{answerId}")
+    @DeleteMapping("/{answerId}")
     public ResponseEntity<Answer> deleteAnswerById(@PathVariable(name = "questionId") Long questionId,
                                                    @PathVariable(name = "answerId") Long answerId) {
-        Question question = getQuestionById(questionId).getBody();
-        assert question != null;
+        Question question = questionService.getById(questionId).get();
         List<Answer> answers = question.getAnswers();
         for (Answer answer : answers) {
             if (answer.getId().equals(answerId)) {
-                answerDao.delete(answer);
+                answerService.delete(answer);
                 return new ResponseEntity<>(answer, HttpStatus.OK);
             }
         }
