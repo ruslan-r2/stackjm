62a1f2b051c45ba2f876dc800e91fa32f2e07aa8
niksergei <niksergei@mail.ru>
2021-08-19 10:16:08 +0300

    @ControllerAdvice возвращает статус INTERNAL_SERVER_ERROR  и сообщение

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/advice/AdviceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/advice/AdviceController.java
@@ -36,5 +36,6 @@ public class AdviceController {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+
     }
 }
