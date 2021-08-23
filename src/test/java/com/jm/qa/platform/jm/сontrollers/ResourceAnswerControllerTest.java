819319e50c9093c0eb9477c3a14dc5ccbd295291
Almaz <zalyalov.almaz2016@gmail.com>
2021-08-23 13:35:31 +0300

    deleteAnswerById test in progress

--- src/test/java/com/jm/qa/platform/jm/сontrollers/ResourceAnswerControllerTest.java
+++ src/test/java/com/jm/qa/platform/jm/сontrollers/ResourceAnswerControllerTest.java
@@ -1,5 +1,9 @@
 package com.jm.qa.platform.jm.сontrollers;
 
+import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
+import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
+import com.javamentor.qa.platform.service.impl.TestDataInitService;
 import com.jm.qa.platform.jm.AbstractIntegrationTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -13,12 +17,19 @@ class ResourceAnswerControllerTest extends AbstractIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private AnswerService answerService;
+    @Autowired
+    private QuestionService questionService;
+    @Autowired
+    private TestDataInitService testDataInitService;
 
     String URL = "api/user/question/{questionId}/answer/{answerId}";
 
     @Test
     void deleteAnswerById() throws Exception {
-
+        testDataInitService.createEntity();
+        Question question = questionService.getById(1L).get();
     }
 
     @AfterEach
