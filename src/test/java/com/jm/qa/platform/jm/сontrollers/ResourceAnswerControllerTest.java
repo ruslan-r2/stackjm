837018455ff0d8c28bf50512644153b57b92b060
Almaz <zalyalov.almaz2016@gmail.com>
2021-08-24 14:45:04 +0300

    in proccess

--- src/test/java/com/jm/qa/platform/jm/сontrollers/ResourceAnswerControllerTest.java
+++ src/test/java/com/jm/qa/platform/jm/сontrollers/ResourceAnswerControllerTest.java
@@ -5,40 +5,41 @@ import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
 import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
 import com.javamentor.qa.platform.service.impl.TestDataInitService;
+import com.javamentor.qa.platform.webapp.controllers.rest.ResourceAnswerController;
 import com.jm.qa.platform.jm.AbstractIntegrationTest;
+import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.List;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class ResourceAnswerControllerTest extends AbstractIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    private AnswerService answerService;
-    @Autowired
-    private QuestionService questionService;
-    @Autowired
-    private TestDataInitService testDataInitService;
+    @InjectMocks
+    private ResourceAnswerController resourceAnswerController;
+    private String url = "api/user/question/{questionId}/answer/{answer}";
 
-    String URL = "api/user/question/{questionId}/answer/{answerId}";
+    @Test
+    public void controllerInitializedCorrectly() {
+        assertThat(resourceAnswerController).isNotNull();
+    }
 
     @Test
     void deleteAnswerById() throws Exception {
-        testDataInitService.createEntity();
-        Question question = questionService.getById(1L).get();
-        List<Answer> answers = question.getAnswers();
-
-    }
-
-    @AfterEach
-    private void resetDb() {
 
     }
 }
