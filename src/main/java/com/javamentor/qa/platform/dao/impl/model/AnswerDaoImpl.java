cda74460a88e138cfcfed0a3e70ae522f961d183
Almaz <zalyalov.almaz2016@gmail.com>
2021-08-30 14:01:42 +0300

    entityManager in AnswerDaoImpl class maked final

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/AnswerDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/AnswerDaoImpl.java
@@ -11,7 +11,7 @@ import java.lang.reflect.ParameterizedType;
 @Repository
 public class AnswerDaoImpl extends ReadWriteDaoImpl<Answer, Long> implements AnswerDao {
 
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
     @Autowired
     public AnswerDaoImpl(EntityManager entityManager) {
