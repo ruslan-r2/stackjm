94567120fc6d297f169153bdbc2d5fdc4b919641
Almaz <zalyalov.almaz2016@gmail.com>
2021-09-02 13:12:25 +0300

    deleteAnswerById documented

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
@@ -2,6 +2,7 @@ package com.javamentor.qa.platform.webapp.controllers.rest;
 
 import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
 import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/api/user/question/{questionId}/answer")
@@ -18,6 +20,9 @@ public class ResourceAnswerController {
     private final QuestionService questionService;
     private final AnswerService answerService;
 
+    @Operation(summary = "Удаление вопроса по Id")
+    @ApiResponse(responseCode = "200", description = "Вопрос помечен на удаление")
+    @ApiResponse(responseCode = "400", description = "Вопрос не найден")
     @DeleteMapping("/{answerId}")
     public ResponseEntity<?> deleteAnswerById(@PathVariable(name = "questionId") Long questionId,
                                               @PathVariable(name = "answerId") Long answerId) {
