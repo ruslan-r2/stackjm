b5081a613665704e7d9f74e61dd7cde2e7e27c46
Almaz <zalyalov.almaz2016@gmail.com>
2021-09-02 17:57:20 +0300

    deleteAnswerById checking isDeleted flag must be true

--- src/test/java/com/jm/qa/platform/jm/сontrollers/ResourceAnswerControllerTest.java
+++ src/test/java/com/jm/qa/platform/jm/сontrollers/ResourceAnswerControllerTest.java
@@ -1,6 +1,7 @@
 package com.jm.qa.platform.jm.сontrollers;
 
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.jm.qa.platform.jm.AbstractIntegrationTest;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -8,6 +9,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -19,6 +23,8 @@ class ResourceAnswerControllerTest extends AbstractIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     private String url = "/api/user/question/{questionId}/answer/{answerId}";
 
@@ -33,6 +39,16 @@ class ResourceAnswerControllerTest extends AbstractIntegrationTest {
     public void deleteAnswerById() throws Exception {
         String correctId = "100";
         String incorrectId = "-100";
+
+        String deleteHql = "from Answer answer set answer.isDeleted = true where answer.id = " + correctId;
+        entityManager.createQuery(deleteHql).executeUpdate();
+
+        String checkDeletedHql = "from Answer answer where answer.id = " + correctId;
+        Answer answer = (Answer) entityManager.createQuery(checkDeletedHql).getSingleResult();
+
+        if (!answer.getIsDeleted()) {
+
+        }
 
         mockMvc.perform(delete(url, correctId, correctId)).
                 andDo(print()).
