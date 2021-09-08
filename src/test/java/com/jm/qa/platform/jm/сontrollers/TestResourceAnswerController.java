88da414d950a5f607a8d04bf44ecfdb9a479f70e
villsaint <jackspow18@gmail.com>
2021-09-09 01:26:51 +0300

    finish

--- src/test/java/com/jm/qa/platform/jm/сontrollers/TestResourceAnswerController.java
+++ src/test/java/com/jm/qa/platform/jm/сontrollers/TestResourceAnswerController.java
@@ -2,14 +2,13 @@ package com.jm.qa.platform.jm.сontrollers;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.javamentor.qa.platform.models.dto.AuthenticationRequestDTO;
+import com.jayway.jsonpath.JsonPath;
 import com.jm.qa.platform.jm.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
 import org.springframework.test.web.servlet.MockMvc;
-
-
+import org.springframework.test.web.servlet.MvcResult;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -20,33 +19,34 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class TestResourceAnswerController extends AbstractIntegrationTest {
 
     private String URL = "/api/user/question/{questionId}/answer";
-    private String JWTurl = "/api/auth/token";
+    private String tokenURL = "/api/auth/token";
     private String token;
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-//    @WithMockUser(username = "user@mail.ru", roles = "USER")
-    @DataSet(value = "userResourceController/getAllAnswers.yml", cleanBefore = true
-            , cleanAfter = true
-    )
+    @DataSet(value = "userResourceController/getAllAnswers.yml", cleanBefore = true, cleanAfter = true)
 
     public void getAllAnswers() throws Exception {
         int idWithAnswers = 100;
         int idWithoutAnswers = 101;
         int idIncorrect = -100;
 
-        AuthenticationRequestDTO correct = new AuthenticationRequestDTO("admin@mail.ru","admin");
+        //получение токена
+        AuthenticationRequestDTO correct = new AuthenticationRequestDTO("admin@mail.ru", "admin");
         String json = objectMapper.writeValueAsString(correct);
-        this.mockMvc.perform(post(JWTurl).contentType(MediaType.APPLICATION_JSON).content(json))
-                .andDo(print())
-                .andExpect(status().isOk())
-        .andExpect(content().encoding(token));
+        MvcResult mvcResult =
+                this.mockMvc.perform(post(tokenURL).contentType(MediaType.APPLICATION_JSON).content(json))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andReturn();
+        token = JsonPath.read(mvcResult.getResponse().getContentAsString(), "token");
 
         //Существующий ид вопроса с ответами,ожидание массива из 2-х ответов
-        this.mockMvc.perform(get(URL, idWithAnswers).header("Authorization",token))
+        this.mockMvc.perform(get(URL, idWithAnswers)
+                .header("Authorization", token)
+        )
                 .andDo(print())
-                .andExpect(SecurityMockMvcResultMatchers.authenticated())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.*", hasSize(2))) // величина ожидаемого массива
                 //1 ответ
@@ -68,15 +68,13 @@ public class TestResourceAnswerController extends AbstractIntegrationTest {
                 .andExpect(jsonPath("$[1].countUserReputation", is(0)))
                 .andExpect(jsonPath("$[1].questionId", is(100)));
         // Существующий ид вопроса без ответов, ожидание пустого массива
-        this.mockMvc.perform(get(URL, idWithoutAnswers))
+        this.mockMvc.perform(get(URL, idWithoutAnswers).header("Authorization", token))
                 .andDo(print())
-                .andExpect(SecurityMockMvcResultMatchers.authenticated())
                 .andExpect(status().isOk())
                 .andExpect(content().string("[]"));
         //Не существующий ид вопроса
-        this.mockMvc.perform(get(URL, idIncorrect))
+        this.mockMvc.perform(get(URL, idIncorrect).header("Authorization", token))
                 .andDo(print())
-                .andExpect(SecurityMockMvcResultMatchers.authenticated())
                 .andExpect(status().isNotFound());
     }
 }
