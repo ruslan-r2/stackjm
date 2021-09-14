bab3ade511b092c0207588f69c1509aa22ee7143
Вячеслав <oganesson@bk.ru>
2021-09-14 17:18:23 +0300

    убрал логику в dao

---
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/TagDtoDaoImpl.java
@@ -0,0 +1,33 @@
+package com.javamentor.qa.platform.dao.impl.dto;
+
+import com.javamentor.qa.platform.dao.abstracts.dto.TagDtoDao;
+import com.javamentor.qa.platform.models.dto.RelatedTagDto;
+import org.hibernate.query.Query;
+import org.hibernate.transform.Transformers;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Collections;
+import java.util.List;
+
+@Repository
+public class TagDtoDaoImpl implements TagDtoDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @SuppressWarnings("unchecked")
+    public List<RelatedTagDto> getTopTags() {
+        List<RelatedTagDto> list =  (List<RelatedTagDto>) entityManager.createQuery(
+                "SELECT tag.id AS id, " +
+                        "tag.name AS title, " +
+                        "CAST (tag.questions.size AS long) AS countQuestion " +
+                        "FROM Tag tag ORDER BY tag.questions.size DESC").unwrap(Query.class)
+                .setResultTransformer(Transformers.aliasToBean(RelatedTagDto.class))
+                .setMaxResults(10)
+                .getResultList();
+        return list.isEmpty() ? Collections.emptyList() : list;
+    }
+
+}
--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/TopTagsController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/TopTagsController.java
@@ -1,32 +1,38 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
+import com.javamentor.qa.platform.dao.abstracts.dto.TagDtoDao;
 import com.javamentor.qa.platform.models.dto.RelatedTagDto;
+import com.javamentor.qa.platform.models.dto.UserDto;
 import com.javamentor.qa.platform.service.abstracts.model.TagService;
 import com.javamentor.qa.platform.webapp.converters.TagConverter;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.hibernate.query.Query;
+import org.hibernate.transform.ResultTransformer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @io.swagger.v3.oas.annotations.tags.Tag(name = "Контроллер топ-10 тегов", description = "Api для вывода топ-10 тегов")
 @RequestMapping("/api/user/tag/related")
 public class TopTagsController {
 
-    private final TagService tagService;
+    @Autowired
+    private TagDtoDao tagDtoDao;
 
-    private final TagConverter tagConverter;
-
-    public TopTagsController(TagService tagService, TagConverter tagConverter) {
-        this.tagService = tagService;
-        this.tagConverter = tagConverter;
+    public TopTagsController(TagDtoDao tagDtoDao) {
+        this.tagDtoDao = tagDtoDao;
     }
 
     @Operation(summary = "Возвращает лист содержащий топ-10 тегов")
@@ -34,17 +40,8 @@ public class TopTagsController {
             content = @Content(mediaType = "application/json",
                     schema = @Schema(implementation = RelatedTagDto.class)))
     @GetMapping
-    public ResponseEntity<ArrayList<RelatedTagDto>> getTop10Tags() {
-        ArrayList<RelatedTagDto> list = new ArrayList<>();
-        for (com.javamentor.qa.platform.models.entity.question.Tag tag :
-                tagService.getAll()) {
-            list.add(tagConverter.tagToRelatedTagDto(tag));
-        }
-        list.sort((o1, o2) -> (int) (o2.getCountQuestion() - o1.getCountQuestion()));
-        if (list.size() > 10) {
-            list = (ArrayList<RelatedTagDto>) list.subList(0, 10);
-        }
-        return new ResponseEntity<>(list, HttpStatus.OK);
+    public ResponseEntity<List<RelatedTagDto>> getTop10Tags() {
+        return new ResponseEntity<>(tagDtoDao.getTopTags(), HttpStatus.OK);
     }
 
 }
