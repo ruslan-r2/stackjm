bfbd07f57eb984e5645a200f13e2660cfdf725f9
Almaz <zalyalov.almaz2016@gmail.com>
2021-09-20 16:24:53 +0300

    delete logic done

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
@@ -2,22 +2,28 @@ package com.javamentor.qa.platform.webapp.controllers.rest;
 
 
 import com.javamentor.qa.platform.models.dto.AnswerDto;
+import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.service.abstracts.dto.AnswerDtoService;
+import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
 import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Tag(name = "Answers контроллер", description = "Api для работы с Answers")
@@ -26,10 +32,13 @@ public class ResourceAnswerController {
 
     private final AnswerDtoService answerDtoService;
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
-    public ResourceAnswerController(AnswerDtoService answerDtoService, QuestionService questionService) {
+    @Autowired
+    public ResourceAnswerController(AnswerDtoService answerDtoService, QuestionService questionService, AnswerService answerServic) {
         this.answerDtoService = answerDtoService;
         this.questionService = questionService;
+        this.answerService = answerServic;
     }
 
 
@@ -49,5 +58,17 @@ public class ResourceAnswerController {
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
+    @DeleteMapping("/{answerId}")
+    @Operation(summary = "Помечает ответ на удаление")
+    @ApiResponse(responseCode = "200", description = "Вопрос успешно помечен на удаление")
+    public ResponseEntity<?> markAnswerToDelete(@PathVariable("answerId") Long answerId) {
+        if (answerService.getById(answerId).isPresent()) {
+            Answer answerToDelete = answerService.getById(answerId).get();
+            answerToDelete.setIsDeleted(true);
+            answerService.update(answerToDelete);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
 
 }
