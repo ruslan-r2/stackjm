d6843f35f00540e742adba37f05c4555f947c913
Almaz <zalyalov.almaz2016@gmail.com>
2021-09-20 17:20:28 +0300

    task done

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/AnswerDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/AnswerDaoImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 @Repository
 public class AnswerDaoImpl extends ReadWriteDaoImpl<Answer, Long> implements AnswerDao {
@@ -14,6 +15,7 @@ public class AnswerDaoImpl extends ReadWriteDaoImpl<Answer, Long> implements Ans
     private EntityManager entityManager;
 
     @Override
+    @Transactional
     public void deleteById(Long id) {
         entityManager.createQuery("update Answer as a set a.isDeleted = true where a.id = :id").setParameter("id", id).executeUpdate();
     }
