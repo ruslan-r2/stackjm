cd890e068b34da14106c956ae3bd2755282fd91b
villsaint <jackspow18@gmail.com>
2021-09-20 09:51:36 +0300

    conflicts

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/TagDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/TagDtoDaoImpl.java
@@ -34,10 +34,10 @@ public class TagDtoDaoImpl implements TagDtoDao {
     @SuppressWarnings("unchecked")
     public List<RelatedTagDto> getTopTags() {
         return (List<RelatedTagDto>) entityManager.createQuery(
-                "SELECT tag.id AS id, " +
-                        "tag.name AS title, " +
-                        "CAST (tag.questions.size AS long) AS countQuestion " +
-                        "FROM Tag tag ORDER BY tag.questions.size DESC").unwrap(Query.class)
+                        "SELECT tag.id AS id, " +
+                                "tag.name AS title, " +
+                                "CAST (tag.questions.size AS long) AS countQuestion " +
+                                "FROM Tag tag ORDER BY tag.questions.size DESC").unwrap(Query.class)
                 .setResultTransformer(Transformers.aliasToBean(RelatedTagDto.class))
                 .setMaxResults(10)
                 .getResultList();
