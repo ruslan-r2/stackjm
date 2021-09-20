d4f4f528914cb1fe7f406619a88806c8907d612c
urlem <ksavdonin@gmail.com>
2021-09-21 01:59:46 +0300

    исправление ошибок

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceQuestionController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceQuestionController.java
@@ -16,7 +16,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Optional;
 
