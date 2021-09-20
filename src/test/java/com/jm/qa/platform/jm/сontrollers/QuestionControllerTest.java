e2956a2a0a16ac79ec311f85e20181da291ece4c
niksergei <niksergei@mail.ru>
2021-09-20 20:25:07 +0300

    Реализовать api получение комментариев к вопросу +TEST+ Добавил тест

--- src/test/java/com/jm/qa/platform/jm/сontrollers/QuestionControllerTest.java
+++ src/test/java/com/jm/qa/platform/jm/сontrollers/QuestionControllerTest.java
@@ -14,66 +14,87 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class QuestionControllerTest extends AbstractIntegrationTest {
 
 
+    // проверка получения комментариев к существующему вопросу(question)
+    @DataSet(value = {"resource_question_controller/roles.yml",
+            "resource_question_controller/users.yml",
+            "resource_question_controller/questions.yml",
+            "resource_question_controller/comment.yml",
+            "resource_question_controller/comment_question.yml",
+            "resource_question_controller/answers.yml",
+            "resource_question_controller/reputations.yml"
+    }, cleanBefore = true)
+    @Test
+    public void getAllCommentQuestion() throws Exception {
 
 
- // проверка получения комментариев к существующему вопросу(question)
- @DataSet(value = {"resource_question_controller/roles.yml",
-         "resource_question_controller/users.yml",
-         "resource_question_controller/questions.yml",
-         "resource_question_controller/comment.yml",
-         "resource_question_controller/comment_question.yml",
-         "resource_question_controller/answers.yml",
-         "resource_question_controller/reputations.yml"
- }, cleanBefore = true)
- @Test
- public void getAllCommentQuestion() throws Exception {
+        String token = getToken("test1@mail.ru", "user");
+
+        mockMvc.perform(get("/api/user/question/111/comment")
+                .header("Authorization", token))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].questionId").value(111))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(102))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].text").value("testComment 102 for question id = 111"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].userId").value(101))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].imageLink").value("https://www.kemri.org/wp-content/uploads/2020/01/495-4952535_create-digital-profile-icon-blue-user-profile-icon.png"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].reputation").value(1500))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].questionId").value(111))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(103))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].text").value("testComment 103 for question id = 111"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].userId").value(102))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].imageLink").value("https://www.kemri.org/wp-content/uploads/2020/01/495-4952535_create-digital-profile-icon-blue-user-profile-icon.png"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].reputation").value(800))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].questionId").value(111))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].id").value(104))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].text").value("testComment 104 for question id = 111"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].userId").value(102))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].imageLink").value("https://www.kemri.org/wp-content/uploads/2020/01/495-4952535_create-digital-profile-icon-blue-user-profile-icon.png"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].reputation").value(800));
+    }
+
+    // проверка получения комментариев к НЕ существующему вопросу(question)
+    @DataSet(value = {"resource_question_controller/roles.yml",
+            "resource_question_controller/users.yml",
+            "resource_question_controller/questions.yml",
+            "resource_question_controller/comment.yml",
+            "resource_question_controller/comment_question.yml",
+            "resource_question_controller/answer.yml",
+            "resource_question_controller/reputation.yml"
+    }, cleanBefore = true)
+    @Test
+    public void getAllCommentQuestionNotFound() throws Exception {
+
+        String token = getToken("test1@mail.ru", "user");
+
+        mockMvc.perform(get("/api/user/question/901/comment")
+                .header("Authorization", token))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
+    }
+
+    // проверка получения комментариев, если их нету
+    @DataSet(value = {"resource_question_controller/roles.yml",
+            "resource_question_controller/users.yml",
+            "resource_question_controller/questions.yml",
+            "resource_question_controller/comment.yml",
+            "resource_question_controller/comment_question.yml",
+            "resource_question_controller/answer.yml",
+            "resource_question_controller/reputation.yml"
+    }, cleanBefore = true)
+    @Test
+    public void getEmptyListCommentQuestion() throws Exception {
+
+        String token = getToken("test1@mail.ru", "user");
+
+        mockMvc.perform(get("/api/user/question/901/comment")
+                .header("Authorization", token))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+    }
 
 
-  String token = getToken("test1@mail.ru", "user");
-
-  mockMvc.perform(get("/api/user/question/111/comment")
-          .header("Authorization",  token))
-          .andDo(print())
-          .andExpect(status().isOk())
-          .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[0].questionId").value(111))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(102))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[0].text").value("testComment 102 for question id = 111"))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[0].userId").value(101))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[0].imageLink").value("https://www.kemri.org/wp-content/uploads/2020/01/495-4952535_create-digital-profile-icon-blue-user-profile-icon.png"))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[0].reputation").value(1500))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[1].questionId").value(111))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(103))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[1].text").value("testComment 103 for question id = 111"))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[1].userId").value(102))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[1].imageLink").value("https://www.kemri.org/wp-content/uploads/2020/01/495-4952535_create-digital-profile-icon-blue-user-profile-icon.png"))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[1].reputation").value(800))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[2].questionId").value(111))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[2].id").value(104))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[2].text").value("testComment 104 for question id = 111"))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[2].userId").value(102))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[2].imageLink").value("https://www.kemri.org/wp-content/uploads/2020/01/495-4952535_create-digital-profile-icon-blue-user-profile-icon.png"))
-          .andExpect(MockMvcResultMatchers.jsonPath("$[2].reputation").value(800));
- }
-
- // проверка получения комментариев к НЕ существующему вопросу(question)
- @DataSet(value = {"resource_question_controller/roles.yml",
-         "resource_question_controller/users.yml",
-         "resource_question_controller/questions.yml",
-         "resource_question_controller/comment.yml",
-         "resource_question_controller/comment_question.yml",
-         "resource_question_controller/answer.yml",
-         "resource_question_controller/reputation.yml"
- }, cleanBefore = true)
- @Test
- public void getAllCommentQuestionNotFound() throws Exception {
-
-  String token = getToken("test1@mail.ru", "user");
-
-  mockMvc.perform(get("/api/user/question/901/comment")
-          .header("Authorization", token))
-          .andDo(print())
-          .andExpect(status().isBadRequest());
-
- }
- }
+}
