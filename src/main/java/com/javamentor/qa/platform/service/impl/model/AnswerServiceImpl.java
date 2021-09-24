2803c8da24c18a858bc8b57dcc885ebd734af681
Maxim <m.krylov88@mail.ru>
2021-09-24 16:43:31 +0300

    сконвертировал дто

--- src/main/java/com/javamentor/qa/platform/service/impl/model/AnswerServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/AnswerServiceImpl.java
@@ -2,14 +2,11 @@ package com.javamentor.qa.platform.service.impl.model;
 
 import com.javamentor.qa.platform.dao.abstracts.model.AnswerDao;
 import com.javamentor.qa.platform.exception.QuestionException;
-import com.javamentor.qa.platform.models.dto.AnswerDto;
 import com.javamentor.qa.platform.models.entity.question.Question;
 import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.models.entity.user.User;
-import com.javamentor.qa.platform.service.abstracts.dto.AnswerDtoService;
 import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
 import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
-import com.javamentor.qa.platform.webapp.converters.AnswerConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,16 +19,13 @@ public class AnswerServiceImpl extends ReadWriteServiceImpl<Answer, Long> implem
 
     private final AnswerDao answerDao;
     private final QuestionService questionService;
-    private final AnswerDtoService answerDtoService;
-    private final AnswerConverter answerConverter;
+
 
     @Autowired
-    public AnswerServiceImpl(AnswerDao answerDao, QuestionService questionService, AnswerDtoService answerDtoService, AnswerConverter answerConverter) {
+    public AnswerServiceImpl(AnswerDao answerDao, QuestionService questionService) {
         super(answerDao);
         this.answerDao = answerDao;
         this.questionService = questionService;
-        this.answerDtoService = answerDtoService;
-        this.answerConverter = answerConverter;
     }
 
     @Override
