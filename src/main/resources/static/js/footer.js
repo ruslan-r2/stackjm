0e53183ffedf33a27636b7bb80090f45f60b579b
macrom1cro <macromicro37@gmail.com>
2021-09-24 13:37:45 +0300

    подгрузил js

---
+++ src/main/resources/static/js/footer.js
@@ -0,0 +1,5 @@
+
+
+document.body.insertAdjacentHTML("beforeend","<footer id=\"footer\" class=\"site-footer js-footer\" role=\"contentinfo\">\n" +
+    "  <div class=\"site-footer--container\"></div>\n" +
+    "</footer>");
