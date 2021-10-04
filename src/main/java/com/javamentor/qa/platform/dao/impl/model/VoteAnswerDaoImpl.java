d0328750bc7e640cd28b76a680e828943062e417
Александр Аккерман <dreder@icloud.com>
2021-10-05 01:33:25 +0300

    VoteAnswerDaoImpl codestyle refactor

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/VoteAnswerDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/VoteAnswerDaoImpl.java
@@ -18,14 +18,16 @@ public class VoteAnswerDaoImpl extends ReadWriteDaoImpl<VoteAnswer,Long> impleme
 
     @Override
     public Long sumVote(Long answerId) {
-        Query query = entityManager.createQuery("select count(v.voteType) as count from VoteAnswer v  where v.answer.id = :id")
+        Query query = entityManager
+                .createQuery("select count(v.voteType) as count from VoteAnswer v  where v.answer.id = :id")
                 .setParameter("id",answerId);
         return (Long) query.getSingleResult();
     }
 
     @Override
     public Optional<VoteAnswer> getByAnswerIdAndUserId(Long answerId, Long userId) {
-        Query query = entityManager.createQuery("select v from VoteAnswer v where v.answer.id = :ansId and v.user.id = :userId")
+        Query query = entityManager
+                .createQuery("select v from VoteAnswer v where v.answer.id = :ansId and v.user.id = :userId")
                 .setParameter("ansId",answerId)
                 .setParameter("userId",userId);
         return SingleResultUtil.getSingleResultOrNull(query);
