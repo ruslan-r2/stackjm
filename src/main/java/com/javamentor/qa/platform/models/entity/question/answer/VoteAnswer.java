1a0b4f42b79f9c5a73c2f91403bc7778c3b5b59f
Александр Аккерман <dreder@icloud.com>
2021-10-05 21:47:25 +0300

    Changed dataset for enum

--- src/main/java/com/javamentor/qa/platform/models/entity/question/answer/VoteAnswer.java
+++ src/main/java/com/javamentor/qa/platform/models/entity/question/answer/VoteAnswer.java
@@ -8,13 +8,7 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Type;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 
