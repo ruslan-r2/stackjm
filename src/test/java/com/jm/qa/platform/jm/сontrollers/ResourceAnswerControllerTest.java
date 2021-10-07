030ea1d04a53efb57d9e365559ace77e5405627a
Admin <predator.1199@mail.ru>
2021-10-07 21:10:47 +0700

    edit AnswerTestController

--- src/test/java/com/jm/qa/platform/jm/сontrollers/ResourceAnswerControllerTest.java
+++ src/test/java/com/jm/qa/platform/jm/сontrollers/ResourceAnswerControllerTest.java
@@ -32,6 +32,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
