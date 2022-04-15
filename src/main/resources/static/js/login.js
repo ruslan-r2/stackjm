9e89d8a9d8908a8698901d36da81b42bd8d5c71a
Ruslan Repin <ruslan_r@digann.ru>
2022-04-15 13:37:37 +0300

    Загрузка проекта на гитхаб.

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,35 +1,38 @@
 function loginSubmit(e, form) {
-    e.preventDefault()
-    fetch("api/auth/token", {
-        method: "POST",
+    e.preventDefault();
+    fetch('/api/auth/token', {
+        method: 'POST',
         headers: {
-            "Content-Type": "application/json"
+            "Content-type": "application/json; charset=UTF-8"
         },
         body: JSON.stringify(Object.fromEntries(new FormData(form).entries()))
-    }).then(response => processResponse(response))
+    }).then(response => processResponse(response));
 }
 
 function processResponse(response) {
     if (response.status === 200) {
         response.json().then(data => {
-            if (document.cookie.split(';').filter(function(item) {
-                return item.trim().indexOf('token=') === 0
+            if (document.cookie.split(";").filter(function(item) {
+                return item.trim().indexOf("token=") === 0
             }).length) {
-                setCookie('token', '', {'max-age': -1});
+                setCookie("token", "", {"max-age": -1});
             }
-            setCookie('token', data.token, {'max-age': 3600, SameSite: 'strict'});
+            setCookie("token", data.token, {"max-age": 3600, SameSite: "strict"});
             window.location.href = "/main";
-        })
+        });
     } else if (response.status === 403) {
         alert("Неверный email или пароль");
+    } else {
+        response.json().then(data => {
+            console.log(data);
+        });
     }
 }
 
 function setCookie(name, value, options = {}) {
     options = {
         // при необходимости добавьте другие значения по умолчанию
-        path: '/',
-        ...options
+        //path: '/'
     };
 
     if (options.expires instanceof Date) {
